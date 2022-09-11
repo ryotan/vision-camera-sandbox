7d7eef2502d68ac8d886ea99c8e352ebf8faf8c4
ryotan <ryotan@ucb-jp.net>
2022-09-11 14:04:03 +0900

    🔧: Restrict import SafeAreaView from react-native

--- features/camera-recording-app/.eslintrc.js
+++ features/camera-recording-app/.eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
   extends: ['@ryotan-vision-camera-sandbox/eslint-config/native'],
+  rules: {
+    // https://eslint.org/docs/latest/rules/no-restricted-imports
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'react-native',
+            importNames: ['SafeAreaProvider', 'SafeAreaView'],
+            message: 'Please use SafeAreaProvider and SafeAreaView from react-native-safe-area-context instead.',
+          },
+        ],
+      },
+    ],
+  },
 };
