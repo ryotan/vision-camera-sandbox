a925a30188172581fe229f36dda34ccd6a10a938
ryotan <ryotan@ucb-jp.net>
2022-09-13 00:36:05 +0900

    🇹: Add image type

---
+++ features/camera-recording-app/src/@types/image.d.ts
@@ -0,0 +1,18 @@
+declare module '*.png' {
+  import type {ImageSourcePropType} from 'react-native';
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.jpg' {
+  import type {ImageSourcePropType} from 'react-native';
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.svg' {
+  import type React from 'react';
+  import type {SvgProps} from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
