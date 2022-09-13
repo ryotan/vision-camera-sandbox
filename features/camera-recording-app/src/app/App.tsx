c49bfa2712880d6ec7fed6097789a0ad81e05f19
ryotan <ryotan@ucb-jp.net>
2022-09-13 23:06:24 +0900

    🔊: Use React.StrictMode

--- features/camera-recording-app/src/app/App.tsx
+++ features/camera-recording-app/src/app/App.tsx
@@ -1,21 +1,31 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'expo-status-bar';
 import type {FunctionComponent} from 'react';
-import {StyleSheet} from 'react-native';
+import {StrictMode} from 'react';
+import {LogBox, StyleSheet} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {initialWindowMetrics, SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {RootStackNavigator} from './navigators/RootStackNavigator';
 
+if (__DEV__) {
+  LogBox.ignoreLogs([
+    // StrictMode warning from react-native.
+    'Please update the following components: AnimatedComponent',
+  ]);
+}
+
 export const App: FunctionComponent = () => {
   return (
-    <GestureHandlerRootView style={StyleSheet.absoluteFill}>
-      <StatusBar style="dark" animated />
-      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-        <NavigationContainer>
-          <RootStackNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <StrictMode>
+      <GestureHandlerRootView style={StyleSheet.absoluteFill}>
+        <StatusBar style="dark" animated />
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+          <NavigationContainer>
+            <RootStackNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
+    </StrictMode>
   );
 };
